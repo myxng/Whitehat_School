2d6d5edcec5744bce953e0c0fc4d32ee2703e59f
myxng <136455921+myxng@users.noreply.github.com>
2023-09-24 22:18:21 +0900

    packet.c

--- 0917_PCAP_Programming/packet.c
+++ 0917_PCAP_Programming/packet.c
@@ -3,11 +3,12 @@
 #include <pcap.h>
 #include <arpa/inet.h>
 
+
 /* Ethernet header */
 struct ethheader {
-    unsigned char  ether_dhost[6];    /* destination host address */
-    unsigned char  ether_shost[6];    /* source host address */
-    unsigned short ether_type;        /* IP? ARP? RARP? etc */
+  u_char  ether_dhost[6]; /* destination host address */
+  u_char  ether_shost[6]; /* source host address */
+  u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
 };
 
 /* IP Header */
@@ -28,13 +29,13 @@ struct ipheader {
 
 /* TCP Header */
 struct tcpheader {
-    unsigned short tcp_sport;               /* source port */
-    unsigned short tcp_dport;               /* destination port */
-    unsigned int   tcp_seq;                 /* sequence number */
-    unsigned int   tcp_ack;                 /* acknowledgement number */
-    unsigned char  tcp_offx2;               /* data offset, rsvd */
+    u_short tcp_sport;               /* source port */
+    u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
 #define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
-    unsigned char  tcp_flags;
+    u_char  tcp_flags;
 #define TH_FIN  0x01
 #define TH_SYN  0x02
 #define TH_RST  0x04
@@ -44,48 +45,33 @@ struct tcpheader {
 #define TH_ECE  0x40
 #define TH_CWR  0x80
 #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-    unsigned short tcp_win;                 /* window */
-    unsigned short tcp_sum;                 /* checksum */
-    unsigned short tcp_urp;                 /* urgent pointer */
+    u_short tcp_win;                 /* window */
+    u_short tcp_sum;                 /* checksum */
+    u_short tcp_urp;                 /* urgent pointer */
 };
 
-/* Psuedo TCP header */
-struct pseudo_tcp
-{
-        unsigned saddr, daddr;
-        unsigned char mbz;
-        unsigned char ptcl;
-        unsigned short tcpl;
-        struct tcpheader tcp;
-        char payload[1500];
-};
-
-void got_packet(unsigned char *args, const struct pcap_pkthdr *header,
-                              const unsigned char *packet)
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+                              const u_char *packet)
 {
   struct ethheader *eth = (struct ethheader *)packet;
+  printf("Ethernet Header\n");
+  printf("   Src MAC : %02x:%02x:%02x:%02x:%02x:%02x\n", eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2], eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
+  printf("   Dst MAC : %02x:%02x:%02x:%02x:%02x:%02x\n", eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2], eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]);
 
   if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
     struct ipheader * ip = (struct ipheader *)
                            (packet + sizeof(struct ethheader));
-
-    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));
-    printf("         To: %s\n", inet_ntoa(ip->iph_destip));
+    printf("IP Header\n");
+    printf("   Src IP : %s\n", inet_ntoa(ip->iph_sourceip));
+    printf("   Dst IP : %s\n", inet_ntoa(ip->iph_destip));
 
     /* determine protocol */
-    switch(ip->iph_protocol) {
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
+    if (ip->iph_protocol == IPPROTO_TCP)
+    {
+        struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + ip->iph_ihl * 4);
+        printf("TCP Header\n");
+        printf("   Src Port : %d\n", ntohs(tcp->tcp_sport));
+        printf("   Dst Port : %d\n", ntohs(tcp->tcp_dport));
     }
   }
 }
@@ -95,11 +81,11 @@ int main()
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "icmp";
-  bpf_unsigned int32 net;
+  char filter_exp[] = "tcp";
+  bpf_u_int32 net;
 
   // Step 1: Open live pcap session on NIC with name enp0s3
-  handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf);
+  handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
 
   // Step 2: Compile filter_exp into BPF psuedo-code
   pcap_compile(handle, &fp, filter_exp, 0, net);
